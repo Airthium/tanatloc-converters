f11d7842ceb4a75da236d9c96b0bf2a4d5981e98
Simon Garnotel <simon.garnotel@gmail.com>
2022-06-16 13:34:21 +0200

    cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tanatloc converters
+
+Various formats (STEP, DXF, msh, vtu) to GLB
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/simon/Documents/Git/Airthium/tanatloc-dockers/converters
-# Build directory: /home/simon/Documents/Git/Airthium/tanatloc-dockers/converters/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/simon/Documents/Git/Airthium/tanatloc-dockers/converters/build/tests_include-b12d07c.cmake")
