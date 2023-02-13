f2366955f1423790ae8524b48576bf3c7df7c275
Simon Garnotel <simon.garnotel@gmail.com>
2023-02-13 13:26:59 +0100

    test

--- src/dxf/DXFConverter.hpp
+++ src/dxf/DXFConverter.hpp
@@ -14,26 +14,6 @@
 #include "./lib/Polyline.hpp"
 #include "./lib/Spline.hpp"
 
-const std::string TAG0 = "  0";
-const std::string TAG10 = " 10";
-const std::string TAG11 = " 11";
-const std::string TAG20 = " 20";
-const std::string TAG21 = " 21";
-const std::string TAG30 = " 30";
-const std::string TAG31 = " 31";
-const std::string TAG40 = " 40";
-const std::string TAG50 = " 50";
-const std::string TAG51 = " 51";
-const std::string TAG71 = " 71";
-const std::string TAG72 = " 72";
-const std::string TAG73 = " 73";
-const std::string TAG74 = " 74";
-const std::string TAG210 = "210";
-const std::string TAG220 = "220";
-const std::string TAG230 = "230";
-const std::string VERTEX = "VERTEX";
-const std::string SEQEND = "SEQEND";
-
 struct Index {
   std::string type;
   unsigned long index;
